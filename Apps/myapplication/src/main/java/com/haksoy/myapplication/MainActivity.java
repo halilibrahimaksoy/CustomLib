669702354887782912646505766be10711f60fdd
halilibrahimaksoy <halilibrahimaksoy31@gmail.com>
2019-05-22 16:08:08 +0300

    1.0.2 publish with bintray job

--- Apps/myapplication/src/main/java/com/haksoy/myapplication/MainActivity.java
+++ Apps/myapplication/src/main/java/com/haksoy/myapplication/MainActivity.java
@@ -3,11 +3,14 @@ package com.haksoy.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.haksoy.customlib.CustomLib;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CustomLib.d();
     }
 }
