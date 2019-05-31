201ca8b1473822a20a7c43aaa928b7f5dad65317
halilibrahimaksoy <halilibrahimaksoy31@gmail.com>
2019-05-31 09:37:13 +0300

    Added Deleting Examaple library

---
+++ deletingexample/src/main/java/com/haksoy/deletingexample/DeletingExample.java
@@ -0,0 +1,9 @@
+package com.haksoy.deletingexample;
+
+import android.util.Log;
+
+public class DeletingExample {
+    public static void d() {
+        Log.d("DeletingExample","DeletingExample was called");
+    }
+}
